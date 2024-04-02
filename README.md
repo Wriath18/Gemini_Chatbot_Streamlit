9d43db438e4c3baab4027b10e034f82bc2352e3e
Wriath18 <wriath18@gmail.com>
2024-04-02 17:51:23 +0530

    Final Push

---
+++ README.md
@@ -0,0 +1,2 @@
+# Streamlit based Gemini Chatbot
+This chatbot uses Gemini-Pro-1.0 Api
---
+++ main.py
@@ -0,0 +1,162 @@
+import time
+import os
+import joblib
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+new_chat_id = f'{time.time()}'
+MODEL_ROLE = 'ai'
+AI_AVATAR_ICON = '✨'
+
+
+response = False
+prompt_tokens = 0
+completion_tokes = 0
+total_tokens_used = 0
+cost_of_response = 0
+
+st.set_page_config(page_title="Gemini Chatbot", page_icon="🤖", layout="wide")
+
+try:
+    os.mkdir('data/')
+except:
+    pass
+
+
+try:
+    past_chats: dict = joblib.load('data/past_chats_list')
+except:
+    past_chats = {}
+
+with st.sidebar:
+    st.write('# Past Chats')
+    if st.session_state.get('chat_id') is None:
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id] + list(past_chats.keys()),
+            format_func=lambda x: past_chats.get(x, 'New Chat'),
+            placeholder='_',
+        )
+    else:
+        
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id, st.session_state.chat_id] + list(past_chats.keys()),
+            index=1,
+            format_func=lambda x: past_chats.get(x, 'New Chat' if x != st.session_state.chat_id else st.session_state.chat_title),
+            placeholder='_',
+        )
+    
+    # TODO: Give user a chance to name chat
+    st.session_state.chat_title = f'ChatSession-{st.session_state.chat_id}'
+
+st.write('# Chat with Gemini')
+
+
+
+st.title("""[Welcome to A Simple Chatbot 🤖 Made by Sanidhya Goel (Click this title for github repo)](https://github.com/Wriath18) """)
+st.write("This is a react framework based 'Streamlit' Application Running Gemini-Pro-1.0 API for Generative Ai based responses.")
+st.write("This is a prototype project, the api currently in use if free tier, please be carefull of the rate limit during usage")
+st.write("# Chat with Gemini ✨")
+st.write("")
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #f0f0f0; /* Change background color */
+        color: #333; /* Change text color */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+try:
+    st.session_state.messages = joblib.load(
+        f'data/{st.session_state.chat_id}-st_messages'
+    )
+    st.session_state.gemini_history = joblib.load(
+        f'data/{st.session_state.chat_id}-gemini_messages'
+    )
+    print('old cache')
+except:
+    st.session_state.messages = []
+    st.session_state.gemini_history = []
+    print('new_cache made')
+st.session_state.model = genai.GenerativeModel('gemini-pro')
+st.session_state.chat = st.session_state.model.start_chat(
+    history=st.session_state.gemini_history,
+)
+
+for message in st.session_state.messages:
+    with st.chat_message(
+        name=message['role'],
+        avatar=message.get('avatar'),
+    ):
+        st.markdown(message['content'])
+
+if prompt := st.chat_input('Your message here...'):
+    if st.session_state.chat_id not in past_chats.keys():
+        past_chats[st.session_state.chat_id] = st.session_state.chat_title
+        joblib.dump(past_chats, 'data/past_chats_list')
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    st.session_state.messages.append(
+        dict(
+            role='user',
+            content=prompt,
+        )
+    )
+    input_length = len(prompt.split())
+    prompt_tokens = input_length * 2
+    completion_tokens = 50
+    cost_of_response = (prompt_tokens+completion_tokens) * 0.000002
+    response = st.session_state.chat.send_message(
+        prompt,
+        stream=True,
+    )
+    
+    with st.chat_message(
+        name=MODEL_ROLE,
+        avatar=AI_AVATAR_ICON,
+    ):
+        message_placeholder = st.empty()
+        full_response = ''
+        assistant_response = response
+        for chunk in response:
+            # TODO: Chunk missing `text` if API stops mid-stream ("safety"?)
+            for ch in chunk.text.split(' '):
+                full_response += ch + ' '
+                time.sleep(0.05)
+                # Rewrites with a cursor at end
+                message_placeholder.write(full_response + '▌')
+        message_placeholder.write(full_response)
+    st.session_state.messages.append(
+        dict(
+            role=MODEL_ROLE,
+            content=st.session_state.chat.history[-1].parts[0].text,
+            avatar=AI_AVATAR_ICON,
+        )
+    )
+    st.session_state.gemini_history = st.session_state.chat.history
+    # Save to file
+    joblib.dump(
+        st.session_state.messages,
+        f'data/{st.session_state.chat_id}-st_messages',
+    )
+    joblib.dump(
+        st.session_state.gemini_history,
+        f'data/{st.session_state.chat_id}-gemini_messages',
+    )
+
+
+with st.sidebar:
+    st.title("Usage Stats:")
+    st.markdown("""---""")
+    st.write("Promt tokens used :", prompt_tokens)
+    st.write("Completion tokens used :", completion_tokes)
+    st.write("Total tokens used :", total_tokens_used)
+    st.write("Total cost of request: ${:.8f}".format(cost_of_response))
